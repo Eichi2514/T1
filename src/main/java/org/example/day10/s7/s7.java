a6e38e1d9eaf4d06d64a9414bd2f5ded549984ff
源긂ichi2514 <picon1317@gmail.com>
2024-06-20 11:06:10 +0900

    commit

--- src/main/java/org/example/day10/s7/s7.java
+++ src/main/java/org/example/day10/s7/s7.java
@@ -2,17 +2,25 @@ package org.example.day10.s7;
 
 public class s7 {
     public static void main(String[] args) {
-        Article article1 = new Article("dbrjsgur13", "134679zx");
-        Article article2 = new Article("picon13", "123456789a");
-        Article article3 = new Article("Eichi2514","794613qw");
-        Article article4 = new Article("AR3372","1q2w3e4ras");
-        Article article5 = new Article("DBJ1598","illhyhl2");
+        Article[] articles = new Article[10];
+        articles[0] = new Article("dbrjsgur13", "134679zx");
+        articles[1] = new Article("picon13", "123456789a");
+        articles[2] = new Article("Eichi2514","794613qw");
+        articles[3] = new Article("AR3372","1q2w3e4ras");
+        articles[4] = new Article("DBJ1598","illhyhl2");
+        articles[5] = new Article();
+        articles[6] = new Article();
+        articles[7] = new Article();
+        articles[8] = new Article();
+
         System.out.println(" ");
-        article1.PW();
-        article2.ID();
-        article3.ID();
-        article4.PW();
-        article5.ID();
+
+        articles[0].PW();
+        articles[1].PW();
+        articles[2].ID();
+        articles[3].PW();
+        articles[4].ID();
+        articles[5].ID();
 
         // int = 12,402
         // long = 10,366
@@ -25,25 +33,23 @@ class Article {
     String i_id;
     String i_pw;
 
-//    Article() {
-//        ++i_number;
-//        o_number = i_number;
-//        System.out.println(o_number);
-//    }
+    Article() {
+        System.out.println(i_number);
+        i_number++;
+    }
 
     Article(String o_id, String o_pw) {
         this.i_id = o_id;
         this.i_pw = o_pw;
-        ++i_number;
-        o_number = i_number;
-        System.out.println(o_number + " : (ID : " + i_id + ", " + "PW : " + i_pw + ")");
+        System.out.println(i_number + " : (ID : " + i_id + ", " + "PW : " + i_pw + ")");
+        i_number++;
     }
 
     void ID() {
-        System.out.println(o_number +"의 ID : "+i_id);
+        System.out.println("articles " + i_number +"번방의 ID : "+i_id);
     }
 
     void PW() {
-        System.out.println(o_number +"의 PW : "+i_pw);
+        System.out.println("articles " + i_number +"번방의 PW : "+i_pw);
     }
 }
---
+++ src/main/java/org/example/day11/s1/s1.java
@@ -0,0 +1,90 @@
+package org.example.day11.s1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class s1 {
+    public static void main(String[] args) {
+        v4();
+    }
+
+    static void v4() {
+        System.out.println("===v4===");
+
+        List<Article> articles = new ArrayList<>();
+
+        articles.add(new Article());
+        articles.add(new Article());
+        articles.add(new Article());
+        articles.add(new Article());
+
+        for (int i = 0; i < articles.size(); i++) {
+            Article article = articles.get(i);
+            System.out.println(article.id);
+        }
+    }
+
+    static void v3() {
+        System.out.println("===v3===");
+
+        ArrayList<Article> articles = new ArrayList<Article>();
+
+        articles.add(new Article());
+        articles.add(new Article());
+        articles.add(new Article());
+        articles.add(new Article());
+
+        for (int i = 0; i < articles.size(); i++) {
+            Article article = articles.get(i);
+            System.out.println(article.id);
+        }
+    }
+
+    static void v2() {
+        System.out.println("===v2===");
+
+        ArrayList articles = new ArrayList();
+
+        articles.add(new Article());
+        articles.add(new Article());
+        articles.add(new Article());
+        articles.add(new Article());
+
+        for (int i = 0; i < articles.size(); i++) {
+            Article article = (Article) articles.get(i);
+            System.out.println(article.id);
+        }
+    }
+
+    static void v1() {
+        System.out.println("===v1===");
+        Article[] articles = new Article[100];
+        articles[0] = new Article();
+        articles[1] = new Article();
+        articles[2] = new Article();
+
+        System.out.println(articles[0].id);
+        System.out.println(articles[1].id);
+        System.out.println(articles[2].id);
+    }
+}
+
+class Article {
+    static int lastId;
+
+    int id;
+    String regDate;
+
+    static {
+        lastId = 0;
+    }
+
+    Article() {
+        this(lastId + 1, "2024-12-12 12:12:12");
+        lastId++;
+        id = lastId;
+    }
+
+    Article(int id, String regDate) {
+    }
+}
