eafe08aac10d239d59feb2d000cce71a863fd3d6
Eichi2514 <picon1317@gmail.com>
2024-06-21 13:35:53 +0900

    commit

--- src/main/java/org/example/SN/day12/s2/s2.java
+++ src/main/java/org/example/SN/day12/s2/s2.java
@@ -1,5 +1,8 @@
 package org.example.SN.day12.s2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class s2 {
     // 문제 : 아래가 실행되도록 해주세요.
     // 조건 : 배열을 사용할 수 없습니다.
@@ -28,7 +31,9 @@ public class s2 {
 }
 
 class 사람인력관리소 {
-    사람[] 사람들 = new 사람[5];
+//    사람[] 사람들 = new 사람[5]; ***** 배열방식 *****
+//    List<사람> 사람들 = new ArrayList<>(); ***** ArrayList방식 *****
+    Map<Integer,사람> 사람들 = new HashMap<>(); // ***** HashMap방식 *****
 
     int 마지막_사람의_번호 = 0;
 
@@ -39,14 +44,18 @@ class 사람인력관리소 {
         a사람.이름 = 이름;
         a사람.나이 = 나이;
 
-        사람들[번호-1] = a사람;
+//        사람들[번호-1] = a사람; ***** 배열방식 *****
+//        사람들.add(a사람); ***** ArrayList방식 *****
+        사람들.put(번호,a사람); // ***** HashMap방식 *****
 
         System.out.printf("나이가 %d살인 %d번째 사람(%s)이 추가되었습니다.\n", 나이, 번호, 이름);
         마지막_사람의_번호++;
     }
 
     사람 get사람(int 번호) {
-        return 사람들[번호-1];
+//        return 사람들[번호-1]; ***** 배열방식 *****
+//        return 사람들.get(번호-1); ***** ArrayList방식 *****
+        return 사람들.get(번호); // ***** HashMap방식 *****
     }
 }
 
