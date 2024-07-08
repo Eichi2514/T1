143e51905707c40a18716fcf71a00e52232ee33f
Eichi2514 <picon1317@gmail.com>
2024-07-08 09:21:03 +0900

    up120815

--- src/main/java/org/example/SN/Main.java
+++ src/main/java/org/example/SN/Main.java
@@ -1,38 +1,22 @@
 package org.example.SN;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[][] a = new int[11][11];
+        System.out.println(Solution.solution(6));
+    }
+}
+class Solution {
+    public static int solution(int n) {
+        int answer = 0;
+        int max = n*6;
 
-        int last = 0;
-        
-        for (int x = 0; x < 10; x++) {
-            for (int y = 0; y < 10; y++) {
-                a[x][y] = sc.nextInt();
+        for (int i = 1; i <= n && i <= 6; i++) {
+            if (n % i == 0 && 6 % i == 0) {
+
+                answer = max / i / 6;
             }
         }
-        for (int x = 1; x < 10; x++) {
-            for (int y = 1; y < 10; y++) {
-                if (a[x][y] == 0) {
-                    a[x][y] = 9;
-                } else if (a[x][y] == 1) {
-                    y -= 2;
-                    x++;
-                } else if (a[x][y] == 2) {
-                    last = a[x][y];
-                    a[x][y] = 9;
-                    break;
-                }
-            }if (last == 2) break;
-        }
-        for (int x = 0; x < 10; x++) {
-            for (int y = 0; y < 10; y++) {
-                System.out.print(a[x][y] + " ");
-            }
-            System.out.println("");
-        }
+
+        return answer;
     }
 }
