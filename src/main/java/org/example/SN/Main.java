5cc93e91d6f7aa1453ddafb21c1f3b8396e52c9b
Eichi2514 <picon1317@gmail.com>
2024-07-11 14:16:27 +0900

    up120843

--- src/main/java/org/example/SN/Main.java
+++ src/main/java/org/example/SN/Main.java
@@ -2,20 +2,34 @@ package org.example.SN;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Solution.solution(6));
+        int[] a = new int[6];
+        a[0] = 1;
+        a[1] = 2;
+        a[2] = 3;
+        a[3] = 4;
+        a[4] = 5;
+        a[5] = 6;
+//        a[6] = 7;
+//        a[7] = 8;
+//        a[8] = 9;
+//        a[9] = 10;
+        String b = "left";
+        System.out.println(Solution.solution(a, 5));
     }
 }
+
 class Solution {
-    public static int solution(int n) {
+    public static int solution(int[] numbers, int k) {
         int answer = 0;
-        int max = n*6;
+        int index = 0;
 
-        for (int i = 1; i <= n && i <= 6; i++) {
-            if (n % i == 0 && 6 % i == 0) {
+        if (k == 1) return numbers[index];
 
-                answer = max / i / 6;
-            }
+        for (int i = 1; i < k; i++) {
+            index += 2;
+            if (index >= numbers.length) index -= numbers.length;
         }
+        answer = numbers[index];
 
         return answer;
     }
