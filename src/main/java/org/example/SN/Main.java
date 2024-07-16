6321c111d9c21ab6ccfd4fed8630f1e68fe095df
Eichi2514 <picon1317@gmail.com>
2024-07-16 10:46:37 +0900

    문제) 한 번만 등장한 문자

--- src/main/java/org/example/SN/Main.java
+++ src/main/java/org/example/SN/Main.java
@@ -1,13 +1,14 @@
 package org.example.SN;
 
+
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-//        int[] a = new int[6];
-//        a[0] = 1;
-//        a[1] = 2;
-//        a[2] = 3;
+        int[] a = new int[3];
+        a[0] = 10;
+        a[1] = 11;
+        a[2] = 12;
 //        a[3] = 4;
 //        a[4] = 5;
 //        a[5] = 6;
@@ -16,28 +17,27 @@ public class Main {
 //        a[8] = 9;
 //        a[9] = 10;
 //        String b = "left";
-        System.out.println(Solution.solution("as3628800"));
+//        System.out.println(Solution.solution(a,13));
 
     }
 }
-
 class Solution {
-    public static int[] solution(String my_string) {
-        int index = 0;
-        int index2 = 0;
-        for (int i = 0; i < my_string.length(); i++) {
-            if (my_string.charAt(i) % 1 == 0 && my_string.charAt(i) < 97) {
-                index++;
-            }
+    public String solution(String s) {
+        String tmp = "";
+        for (int i = 0; i < s.length(); i++) {
+            int count = -1;
+            for (int j = 0; j < s.length(); j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    count++;
+                }
+            }if (count == 0) tmp += s.charAt(i);
         }
-        int[] answer = new int[index];
-        for (int i = 0; i < my_string.length(); i++) {
-            if (my_string.charAt(i) % 1 == 0 && my_string.charAt(i) < 97) {
-                answer[index2] = my_string.charAt(i) - 48;
-                index2++;
-            }
+        char[] chars = tmp.toLowerCase().toCharArray();
+        Arrays.sort(chars);
+        String answer = "";
+        for (int i = 0; i < chars.length; i++) {
+            answer += chars[i];
         }
-        Arrays.sort(answer);
         return answer;
     }
 }
