0f38c6e1e35c2ac95a1fb88bda59ece18aae36fd
Eichi2514 <picon1317@gmail.com>
2024-07-18 16:54:27 +0900

    프로그래머스 : 다항식 더하기

--- src/main/java/org/example/SN/Main.java
+++ src/main/java/org/example/SN/Main.java
@@ -14,42 +14,50 @@ public class Main {
 //        a[7] = "3 + 21 = 24";
 //        a[8] = "3 + 24 = 27";
 //        a[9] = "3 + 27 = 30";
-        System.out.println(Solution.solution(a,4));
+        System.out.println(Solution.solution("3 + 2 + x + 2 + x"));
     }
 }
 
 class Solution {
-    public static int[] solution(int[] numlist, int n) {
-        int[] answer = new int[numlist.length];
-        int max = n+1;
-        int min = n-1;
-        int index = 0;
-        for (int i = 0; i < numlist.length; i++) {
-            if (n == numlist[i]) {
-                answer[index] = numlist[i];
-                index++;
+    public static String solution(String polynomial) {
+        String answer = "";
+        if (polynomial.charAt(0) == 'x') polynomial = polynomial.replaceAll("x ", "1x ");
+        polynomial = polynomial.replaceAll(" x", " 1x");
+        String[] numbers = polynomial.split(" \\+ ");
+        int xIndex = 0;
+        int nIndex = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i].contains("x")) xIndex++;
+            else nIndex++;
+        }
+        String[] x = new String[xIndex];
+        String[] n = new String[nIndex];
+        xIndex = 0;
+        nIndex = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i].contains("x")) {
+                x[xIndex] = numbers[i].replace("x","");
+                xIndex++;
+            } else {
+                n[nIndex] = numbers[i];
+                nIndex++;
             }
         }
-        while (true) {
-            if (max <= 10000) {
-                for (int i = 0; i < numlist.length; i++) {
-                    if (max == numlist[i]) {
-                        answer[index] = numlist[i];
-                        index++;
-                    }
-                }
-                max++;
-            }
-            if (min > 0) {
-                for (int i = 0; i < numlist.length; i++) {
-                    if (min == numlist[i]) {
-                        answer[index] = numlist[i];
-                        index++;
-                    }
-                }
-                min--;
-            } else if (max == 10001 && min == 0) break;
+        int xr = 0;
+        int nr = 0;
+        for (int i = 0; i < xIndex; i++) {
+            xr += Integer.parseInt(x[i]);
         }
+        for (int i = 0; i < nIndex; i++) {
+            nr += Integer.parseInt(n[i]);
+        }
+        if(numbers[0].contains("x")){
+            answer = Integer.toString(xr) + "x + " + Integer.toString(nr);
+        }else answer = Integer.toString(nr) + " + " + Integer.toString(xr) + "x";
+        answer = answer.replaceAll(" \\+ 0x", "");
+        answer = answer.replaceAll(" \\+ 0", "");
+        answer = answer.replaceAll("0x \\+ ", "");
+        answer = answer.replaceAll("0 \\+ ", "");
         return answer;
     }
 }
