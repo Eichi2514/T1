8c8a9233df74bf8fc2e74d6859a85ed4f59fc56d
Eichi2514 <picon1317@gmail.com>
2024-07-22 13:13:58 +0900

    백준 : 공넣기

--- src/main/java/org/example/SN/Main.java
+++ src/main/java/org/example/SN/Main.java
@@ -7,19 +7,25 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int max = Integer.parseInt(bf.readLine());
-        int i = 1;
-        int count = i;
-        String s;
-        while ((s = bf.readLine()) != null){
-            i++;
-            if (max <= Integer.parseInt(s)){
-                max = Integer.parseInt(s);
-                count = i;
+        String[] tmp = bf.readLine().split(" ");
+        int number = Integer.parseInt(tmp[0]);
+        int max = Integer.parseInt(tmp[1]);
+        int[] bools = new int[number];
+        for (int i = 0; i < number; i++) {
+            bools[i] = 0;
+        }
+        for (int i = 0; i < max; i++) {
+            tmp = bf.readLine().split(" ");
+            int start = Integer.parseInt(tmp[0]);
+            int end = Integer.parseInt(tmp[1]);
+            int bool = Integer.parseInt(tmp[2]);
+            for (int j = start - 1; j < end; j++) {
+                bools[j] = bool;
             }
         }
-        bw.write(max + " " + count);
-        bw.newLine();
+        for (int i = 0; i < number; i++) {
+            bw.write(bools[i] + " ");
+        }
         bw.flush();
     }
 }
