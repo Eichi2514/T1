2c6c3f825adf14e27cb4310945a76f2f2c09c15f
Eichi2514 <picon1317@gmail.com>
2024-07-22 09:15:58 +0900

    백준 : 최댓값

--- src/main/java/org/example/SN/Main.java
+++ src/main/java/org/example/SN/Main.java
@@ -7,23 +7,19 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(bf.readLine());
-        String[] tmp = bf.readLine().split(" ");
-        int[] numbers = new int[n];
-        for (int i = 0; i < n; i++) {
-            numbers[i] = Integer.parseInt(tmp[i]);
-        }
-        int max = numbers[0];
-        int min = numbers[0];
-        for (int i = 0; i < n-1; i++) {
-            if (max <= numbers[i+1]) {
-                max = numbers[i+1];
-            }
-            if (min >= numbers[i+1]) {
-                min = numbers[i+1];
+        int max = Integer.parseInt(bf.readLine());
+        int i = 1;
+        int count = i;
+        String s;
+        while ((s = bf.readLine()) != null){
+            i++;
+            if (max <= Integer.parseInt(s)){
+                max = Integer.parseInt(s);
+                count = i;
             }
         }
-        bw.write(min + " " + max);
+        bw.write(max + " " + count);
+        bw.newLine();
         bw.flush();
     }
 }
