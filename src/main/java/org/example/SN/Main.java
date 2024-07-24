44b9181fc9982d1ba5d8ea318c18c0ac0423b57c
Eichi2514 <picon1317@gmail.com>
2024-07-24 11:39:58 +0900

    백준 : 문자열 반복

--- src/main/java/org/example/SN/Main.java
+++ src/main/java/org/example/SN/Main.java
@@ -2,25 +2,22 @@ package org.example.SN;
 
 
 import java.io.*;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(bf.readLine());
-        String[] tmp = bf.readLine().split(" ");
-        double[] tmpN = new double[n];
-        double sum = 0;
         for (int i = 0; i < n; i++) {
-            tmpN[i] += Integer.parseInt(tmp[i]);
+            String[] tmp = bf.readLine().split(" ");
+            int x = Integer.parseInt(tmp[0]);
+            for (int j = 0; j < tmp[1].length(); j++) {
+                for (int k = 0; k < x; k++) {
+                    bw.write(tmp[1].charAt(j));
+                }
+            }
+            bw.newLine();
         }
-        Arrays.sort(tmpN);
-        double max = tmpN[tmpN.length - 1];
-        for (int i = 0; i < n; i++) {
-            sum += tmpN[i] / max;
-        }
-        bw.write((sum * 100 / n) + "");
         bw.flush();
     }
 }
