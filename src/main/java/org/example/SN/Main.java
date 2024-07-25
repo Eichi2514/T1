c4916204d2892c571aebc08ce29efceb7ac68332
Eichi2514 <picon1317@gmail.com>
2024-07-25 14:12:50 +0900

    백준 : 다이얼

--- src/main/java/org/example/SN/Main.java
+++ src/main/java/org/example/SN/Main.java
@@ -7,20 +7,21 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String[] tmps = bf.readLine().split(" ");
-        int[] n = new int[tmps.length];
-        int max;
-        for (int i = 0; i < tmps.length; i++) {
-            String tmp = "";
-            for (int j = tmps[i].length()-1; j >= 0; j--) {
-                tmp+=tmps[i].charAt(j);
-            } n[i] = Integer.parseInt(tmp);
+        String tmp = bf.readLine();
+        int count = 0;
+        for (int i = 0; i < tmp.length(); i++) {
+            if (tmp.charAt(i) == 'A' || tmp.charAt(i) == 'B' || tmp.charAt(i) == 'C') count += 3;
+            else if (tmp.charAt(i) == 'D' || tmp.charAt(i) == 'E' || tmp.charAt(i) == 'F') count += 4;
+            else if (tmp.charAt(i) == 'G' || tmp.charAt(i) == 'H' || tmp.charAt(i) == 'I') count += 5;
+            else if (tmp.charAt(i) == 'J' || tmp.charAt(i) == 'K' || tmp.charAt(i) == 'L') count += 6;
+            else if (tmp.charAt(i) == 'M' || tmp.charAt(i) == 'N' || tmp.charAt(i) == 'O') count += 7;
+            else if (tmp.charAt(i) == 'P' || tmp.charAt(i) == 'Q' || tmp.charAt(i) == 'R' || tmp.charAt(i) == 'S')
+                count += 8;
+            else if (tmp.charAt(i) == 'T' || tmp.charAt(i) == 'U' || tmp.charAt(i) == 'V') count += 9;
+            else if (tmp.charAt(i) == 'W' || tmp.charAt(i) == 'X' || tmp.charAt(i) == 'Y' || tmp.charAt(i) == 'Z')
+                count += 10;
         }
-        max = n[0];
-        for (int i = 0; i < n.length; i++) {
-            if (max < n[i]) max = n[i];
-        }
-        bw.write(max + "");
+        bw.write(count + "");
         bw.flush();
     }
 }
