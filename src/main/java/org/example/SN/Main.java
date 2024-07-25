b547096fb3e9edca47c7f3e3d2005c78c3f8df5b
Eichi2514 <picon1317@gmail.com>
2024-07-25 09:18:50 +0900

    백준 : 상수

--- src/main/java/org/example/SN/Main.java
+++ src/main/java/org/example/SN/Main.java
@@ -7,17 +7,20 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(bf.readLine());
-        for (int i = 0; i < n; i++) {
-            String[] tmp = bf.readLine().split(" ");
-            int x = Integer.parseInt(tmp[0]);
-            for (int j = 0; j < tmp[1].length(); j++) {
-                for (int k = 0; k < x; k++) {
-                    bw.write(tmp[1].charAt(j));
-                }
-            }
-            bw.newLine();
+        String[] tmps = bf.readLine().split(" ");
+        int[] n = new int[tmps.length];
+        int max;
+        for (int i = 0; i < tmps.length; i++) {
+            String tmp = "";
+            for (int j = tmps[i].length()-1; j >= 0; j--) {
+                tmp+=tmps[i].charAt(j);
+            } n[i] = Integer.parseInt(tmp);
         }
+        max = n[0];
+        for (int i = 0; i < n.length; i++) {
+            if (max < n[i]) max = n[i];
+        }
+        bw.write(max + "");
         bw.flush();
     }
 }
