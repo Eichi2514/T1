d3c3305448c1a2d5772116d9203b812d9c46b7c6
Eichi2514 <picon1317@gmail.com>
2024-07-29 14:22:26 +0900

    프로그래머스 : 공백으로 구분하기 2

--- src/main/java/org/example/SN/Main.java
+++ src/main/java/org/example/SN/Main.java
@@ -1,21 +1,27 @@
 package org.example.SN;
 
-import java.io.*;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        System.out.println(Solution.solution(" "));
     }
 }
 
 class Solution {
-    public int solution(int n) {
-        int answer = 0;
-        if (n%2 == 0) {
-            for (int i = 2; i <= n; i+=2) {
-                    answer += i*i;
+    public static String[] solution(String my_string) {
+        String[] answer2 = my_string.split(" ");
+        int index = 0;
+        for (int i = 0; i < answer2.length; i++) {
+            if (!(answer2[i].equals(""))) index++;
+        }
+        String[] answer = new String[index];
+        index = 0;
+        for (int i = 0; i < answer2.length; i++) {
+            if (!(answer2[i].equals(""))) {
+                answer[index] = answer2[i];
+                index++;
             }
-        } else for (int i = 1; i <= n; i+=2) {
-            answer += i;
         }
         return answer;
     }
