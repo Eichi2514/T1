f6c984f6d250c3233b52678b1960ce86b1d4441d
Eichi2514 <picon1317@gmail.com>
2024-07-29 09:02:05 +0900

    프로그래머스 : 홀짝에 따라 다른 값 반환하기

--- src/main/java/org/example/SN/Main.java
+++ src/main/java/org/example/SN/Main.java
@@ -8,11 +8,15 @@ public class Main {
 }
 
 class Solution {
-    public int solution(int a, int b) {
+    public int solution(int n) {
         int answer = 0;
-        String ab = a + "" + b;
-        if (Integer.parseInt(ab) >= (2 * a * b)) answer = Integer.parseInt(ab);
-        else answer = 2 * a * b;
+        if (n%2 == 0) {
+            for (int i = 2; i <= n; i+=2) {
+                    answer += i*i;
+            }
+        } else for (int i = 1; i <= n; i+=2) {
+            answer += i;
+        }
         return answer;
     }
 }
