5ca3dac6bd4caf040e2166e292aca13dc5b4295c
Eichi2514 <picon1317@gmail.com>
2024-07-30 09:20:46 +0900

    프로그래머스 : 코드 처리하기

--- src/main/java/org/example/SN/Main.java
+++ src/main/java/org/example/SN/Main.java
@@ -4,25 +4,27 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println(Solution.solution(" "));
+        int[] s = {4, 2, 6, 1, 7, 6};
+
+        System.out.println(Solution.solution("1"));
     }
 }
-
 class Solution {
-    public static String[] solution(String my_string) {
-        String[] answer2 = my_string.split(" ");
-        int index = 0;
-        for (int i = 0; i < answer2.length; i++) {
-            if (!(answer2[i].equals(""))) index++;
-        }
-        String[] answer = new String[index];
-        index = 0;
-        for (int i = 0; i < answer2.length; i++) {
-            if (!(answer2[i].equals(""))) {
-                answer[index] = answer2[i];
-                index++;
+    public static String solution(String code) {
+        String answer = "";
+        int mod = 0;
+        for (int i = 0; i < code.length(); i++) {
+            if (mod == 0) {
+                if (code.charAt(i) == '1') {
+                    mod = 1;
+                } else if (i % 2 == 0) answer += code.charAt(i);
+            } else if (mod == 1) {
+                if (code.charAt(i) == '1') {
+                    mod = 0;
+                } else if (i % 2 != 0) answer += code.charAt(i);
             }
         }
+        if (answer.equals("")) return "EMPTY";
         return answer;
     }
 }
