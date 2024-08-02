cdbf985bf4940525f6e9733139723c8100c10f35
Eichi2514 <picon1317@gmail.com>
2024-08-02 09:06:07 +0900

    프로그래머스 : 9로 나눈 나머지

--- src/main/java/org/example/SN/Main.java
+++ src/main/java/org/example/SN/Main.java
@@ -1,26 +1,21 @@
 package org.example.SN;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         int[] s = {1, 4, 2, 5, 3};
-        System.out.println(Solution.solution(s));
+        System.out.println(Solution.solution("78720646226947352489"));
     }
 }
 
 class Solution {
-    public static ArrayList<Integer> solution(int[] arr) {
-        ArrayList<Integer> stk = new ArrayList<>();
-        for (int i = 0; i < arr.length; i++) {
-            if (stk.size() == 0) stk.add(arr[i]);
-            else if (stk.get(stk.size() - 1) < arr[i]) stk.add(arr[i]);
-            else if (stk.get(stk.size() - 1) >= arr[i]) {
-                stk.remove(stk.size() - 1);
-                i--;
-            }
+    public static int solution(String number) {
+        int answer = 0;
+        for (int i = 0; i < number.length(); i++) {
+            answer += number.charAt(i)-48;
         }
-        return stk;
+        answer = answer % 9;
+        return answer;
     }
 }
