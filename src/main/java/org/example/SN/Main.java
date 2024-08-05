4fd9b7348e47da17a818b8d58aca6ca94fe1ab13
Eichi2514 <picon1317@gmail.com>
2024-08-05 09:04:08 +0900

    프로그래머스 : 배열 조각하기

--- src/main/java/org/example/SN/Main.java
+++ src/main/java/org/example/SN/Main.java
@@ -1,21 +1,34 @@
 package org.example.SN;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int[] s = {1, 4, 2, 5, 3};
-        System.out.println(Solution.solution("78720646226947352489"));
+        int[] arr = {0, 1, 2, 3, 4, 5};
+        int[] query = {2, 1, 2};
+        System.out.println(Solution.solution(arr, query));
     }
 }
 
 class Solution {
-    public static int solution(String number) {
-        int answer = 0;
-        for (int i = 0; i < number.length(); i++) {
-            answer += number.charAt(i)-48;
+    public static ArrayList<Integer> solution(int[] arr, int[] query) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            answer.add(arr[i]);
         }
-        answer = answer % 9;
+        for (int i = 0; i < query.length; i++) {
+            int n = answer.size();
+            if (i % 2 == 0) {
+                for (int j = query[i] + 1; j < n; j++) {
+                    answer.remove(answer.size() - 1);
+                }
+            } else {
+                for (int j = 0; j < query[i]; j++) {
+                    answer.remove(0);
+                }
+            }
+        }
         return answer;
     }
 }
